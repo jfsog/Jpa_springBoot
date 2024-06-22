bd164e8b45d6ba72bbc55952c709aa0dbcdd4b10
jfsog <Felipe.sg@protonmail.com>
2024-06-22 10:01:27 -0300

    Before derivate query methods

---
+++ src/main/java/com/spikixi/jpa/Entidades/embedded/OrderId.java
@@ -0,0 +1,17 @@
+package com.spikixi.jpa.Entidades.embedded;
+
+import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Embeddable
+public class OrderId {
+    private String username;
+    private LocalDateTime orderDate;
+}
